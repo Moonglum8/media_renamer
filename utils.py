55fd597c00d376e0e8b1eb58bf6ace00c32f04aa
Moonglum8 <michael_tagg@hotmail.com>
2026-02-09 13:27:07 +0000

    feat: refactor file renaming logic and enhance output display
    - Updated the naming function to include movie titles and release years from TMDB API.
    - Improved the output display using a table format for better readability.
    - Removed command-line argument handling in favor of using Typer for command management.

--- utils.py
+++ utils.py
@@ -1,15 +1,23 @@
 import os
-import sys
 import typing
+
 import requests
-import json
-from pprint import pprint
+import typer
+
 from dotenv import load_dotenv
 from urllib.parse import quote
+
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from rich.markup import escape
 
 load_dotenv()
 READ_ONLY_TOKEN = os.getenv("READ_ONLY_TOKEN")
 API_KEY = os.getenv("API_KEY")
+
+console = Console(markup=False)
+app = typer.Typer()
 
 def get_file_names(folder: str, extension: str = "mkv") -> typing.List[tuple]:
 
@@ -31,8 +39,10 @@ def tmdb_api_movie_search(query_str: str) -> dict:
     return response.json()
 
 
-def make_new_namesList(file_names: typing.List[tuple]) -> typing.List[tuple[str, str]]:
+def make_new_names_list_movie(file_names: typing.List[tuple]) -> typing.List[tuple[str, str]]:
     new_names: typing.List[tuple[str, str]] = []
+    print("")
+    print("Processing file names...")
     for name, ext in file_names:
         old_name: str = f"{name}.{ext}"
         new_name: str = old_name
@@ -41,8 +51,11 @@ def make_new_namesList(file_names: typing.List[tuple]) -> typing.List[tuple[str,
             try:
                 new_name: str = f"{tmdb_obj['results'][0]['title']} ({tmdb_obj['results'][0]['release_date'][:4]}) [tmdbid-{tmdb_obj['results'][0]['id']}].{ext}"
             except (KeyError, IndexError):
-                pass 
-        new_names.append((old_name, new_name))     
+                pass
+        print(escape(f"{old_name} -> {new_name}"), "*** Unchanged ***" if old_name == new_name else "")
+        new_names.append((old_name, new_name))
+    print("Done.")
+    print("")
     return new_names
 
 
@@ -52,17 +65,19 @@ def rename_files(folder: str, new_names: typing.List[tuple[str, str]]) -> None:
         new_path = os.path.join(folder, new_name)
         os.rename(old_path, new_path)
 
+@app.command()
+def rename_movies(folder: str, extension: str = "mkv", dry_run: bool = True) -> None:
+    file_names = get_file_names(folder, extension)
+    new_names = make_new_names_list_movie(file_names)
+    table = Table("Old", "New")
+
+    for old, new in new_names:
+        table.add_row(old, new)
+ 
+    console.print(table)
+    if not dry_run:
+        rename_files(folder, new_names)
+
 
 if __name__ == "__main__":
-    pprint(sys.argv)
-    if len(sys.argv) < 2:
-        print("Usage: python utils.py <folder_path> [extension]")
-        sys.exit(1)
-
-    folder_path = sys.argv[1]
-    extension = sys.argv[2] if len(sys.argv) > 2 else "mkv"
-    file_names = get_file_names(folder_path, extension)
-    # pprint(file_names)
-
-    new_names = make_new_namesList(file_names)
-    pprint(new_names)
+    app()
