6d75d2b7b28d230a4bb48b626881d819fb9ad1ae
Moonglum8 <michael_tagg@hotmail.com>
2026-02-09 15:30:26 +0000

    feat: enhance file renaming functionality and improve output
    - Updated `make_new_names_list_movie` to include file extensions in the new names list.
    - Modified `rename_files` to handle creation of media folders and improved path handling.
    - Enhanced console output for better user feedback during processing and renaming.

--- utils.py
+++ utils.py
@@ -11,6 +11,7 @@ from rich import print
 from rich.console import Console
 from rich.table import Table
 from rich.markup import escape
+from rich.progress import track
 
 load_dotenv()
 READ_ONLY_TOKEN = os.getenv("READ_ONLY_TOKEN")
@@ -39,45 +40,61 @@ def tmdb_api_movie_search(query_str: str) -> dict:
     return response.json()
 
 
-def make_new_names_list_movie(file_names: typing.List[tuple]) -> typing.List[tuple[str, str]]:
-    new_names: typing.List[tuple[str, str]] = []
+def make_new_names_list_movie(file_names: typing.List[tuple]) -> typing.List[tuple[str, str, str]]:
+    new_names: typing.List[tuple[str, str, str]] = []
     print("")
-    print("Processing file names...")
+    print("[green]Processing file names...")
     for name, ext in file_names:
-        old_name: str = f"{name}.{ext}"
+        old_name: str = f"{name}"
         new_name: str = old_name
         tmdb_obj = tmdb_api_movie_search(name)
         if tmdb_obj["results"]:
             try:
-                new_name: str = f"{tmdb_obj['results'][0]['title']} ({tmdb_obj['results'][0]['release_date'][:4]}) [tmdbid-{tmdb_obj['results'][0]['id']}].{ext}"
+                new_name: str = f"{tmdb_obj['results'][0]['title']} ({tmdb_obj['results'][0]['release_date'][:4]}) [tmdbid-{tmdb_obj['results'][0]['id']}]"
             except (KeyError, IndexError):
                 pass
         print(escape(f"{old_name} -> {new_name}"), "*** Unchanged ***" if old_name == new_name else "")
-        new_names.append((old_name, new_name))
-    print("Done.")
+        new_names.append((old_name, new_name, ext))
+    print("[green]Done.")
     print("")
     return new_names
 
 
-def rename_files(folder: str, new_names: typing.List[tuple[str, str]]) -> None:
-    for old_name, new_name in new_names:
-        old_path = os.path.join(folder, old_name)
-        new_path = os.path.join(folder, new_name)
+def file_ext(file_name: str, extension: str) -> str:
+    return f"{file_name}.{extension}"
+
+
+def rename_files(folder: str, new_names: typing.List[tuple[str, str, str]], create_media_folder: bool = False) -> None:
+    for old_name, new_name, ext in track(new_names, description="[green]Renaming files..."):
+        old_path = os.path.join(folder, file_ext(old_name, ext))
+        if create_media_folder:
+            new_path = os.path.join(folder, new_name)
+            if not os.path.exists(new_path):
+                os.makedirs(new_path)
+            new_path = os.path.join(new_path, file_ext(new_name, ext))
+        else:
+            new_path = os.path.join(folder, file_ext(new_name, ext))
         os.rename(old_path, new_path)
 
+
 @app.command()
-def rename_movies(folder: str, extension: str = "mkv", dry_run: bool = True) -> None:
+def rename_movies(folder: str, extension: str = "mkv", dry_run: bool = True, create_media_folder: bool = False) -> None:
     file_names = get_file_names(folder, extension)
     new_names = make_new_names_list_movie(file_names)
     table = Table("Old", "New")
 
-    for old, new in new_names:
-        table.add_row(old, new)
+    if not dry_run:
+        rename_files(folder, new_names, create_media_folder)
+        print("[green]Done.")
+        print("")
+
+    for old, new, ext in new_names:
+        if create_media_folder:
+            table.add_row(f"{folder}/{file_ext(old, ext)}", f"{folder}/{new}/{file_ext(new, ext)}")
+        else:
+            table.add_row(f"{folder}/{file_ext(old, ext)}", f"{folder}/{file_ext(new, ext)}")
  
     console.print(table)
-    if not dry_run:
-        rename_files(folder, new_names)
-
 
 if __name__ == "__main__":
     app()
